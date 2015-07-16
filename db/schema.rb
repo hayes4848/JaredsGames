df617b3589b0b24cf95c4635e9a3ed7735c03d24
Andy Hayes <andy.hayes@SAX-LT-AHAYES.local>
2015-07-15 23:29:19 -0600

    changes db to postgres.. not using the db but switching it for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150716042046) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "static_pages", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
